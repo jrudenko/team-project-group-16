c46398042b21edf32afbc06deac9adc7c3f6b089
jrudenko <105347404+jrudenko@users.noreply.github.com>
2022-08-12 16:24:19 +0000

    Deploying to gh-pages from @ jrudenko/team-project-group-16@9d192b097510c555ff81d6a5dfb98344db429d76 🚀

---
+++ index.a480e64d.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close-or]"),modal:document.querySelector("[data-modal]"),body:document.querySelector("[data-body]")};function e(){o.modal.classList.toggle("is-hidden"),o.body.classList.toggle("no-scroll")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.a480e64d.js.map
