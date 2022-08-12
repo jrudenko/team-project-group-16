21965346544fa7fa42dc431d4f05e737b77ee779
jrudenko <105347404+jrudenko@users.noreply.github.com>
2022-08-12 16:10:09 +0000

    Deploying to gh-pages from @ jrudenko/team-project-group-16@676a0b6f89cb81dd3c91419d30de8c2a579fd3d7 🚀

---
+++ index.a992cd04.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-modal-open-fr]"),closeModalBtn:document.querySelector("[data-modal-close-fr]"),modal:document.querySelector("[data-modal-fr]"),body:document.querySelector("[data-body]")};function e(){o.modal.classList.toggle("is-hidden"),o.body.classList.toggle("no-scroll")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.a992cd04.js.map
--- index.f49d4505.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={openModalBtn:document.querySelector("[franch-modal-open]"),closeModalBtn:document.querySelector("[franch-modal-close]"),modal:document.querySelector("[franch-modal]"),body:document.querySelector("[data-body]")};function e(){o.modal.classList.toggle("is-hidden"),o.body.classList.toggle("no-scroll")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
-//# sourceMappingURL=index.f49d4505.js.map
